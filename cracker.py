a7f0963cfb5aa8a0c333109507127ea2356eb849
Jonah McElfatrick <jmcelfatrick@gmail.com>
2022-10-30 01:53:40 +0100

    Adding functionality to brute force and dictionary

--- cracker.py
+++ cracker.py
@@ -1,3 +1,4 @@
+from distutils import archive_util
 import hashlib
 import threading
 import string
@@ -28,6 +29,9 @@ symbols = {
 numbers = {
 	'1','2','3','4','5','6','7','8','9','0'}
 
+cracked_method = ""
+cracked = False
+
 def main():
     # Parse arguments
     arguments = parser.parse_args()
@@ -45,24 +49,85 @@ def main():
     # print(result.hexdigest())
 
 # Function to use the inputted dictionary to attempt the crack the hash
-def dictionary_attack():
-    print ("Dictionary Attack")
+def dictionary_attack(arguments):
+    # Check if the hash has been cracked, if so then return from function
+    if (cracked):
+        return
+
+    # Open the wordlist file using read permissions and set contents to variable
+    dictionary = open(arguments.dictionary, "r")
+
+    testing_string = ""
+    # If the user has decided to output all attempts to the cli, then print out attempted string
+    if (arguments.output.lower() == "yes"):
+        print ("Attemping: ", testing_string)
+
+    dictionary.close() 
+
 
 # Function to brute force attack the hash
-def brute_force_attack():
-    print ("Brute Force Attack")
+def brute_force_attack(arguments,testing_string_length, testing_string):
+    # Check if the hash has been cracked, if so then return from function
+    if (cracked):
+        return
+    
+    # If the user has decided to output all attempts to the cli, then print out attempted string
+    if (arguments.output.lower() == "yes"):
+        print ("Attemping: ", testing_string)
+
+    if (arguments.algorithm.lower() == "all" or arguments.algorithm.lower() == "md5"):
+        new_hash = hashlib.md5(testing_string)
+        hash_method = "MD5"
+    elif (arguments.algorithm.lower() == "all" or arguments.algorithm.lower() == "sha1"):
+        new_hash = hashlib.sha1(testing_string)
+        hash_method = "SHA1"
+    elif (arguments.algorithm.lower() == "all" or arguments.algorithm.lower() == "sha224"):
+        new_hash = hashlib.sha224(testing_string)
+        hash_method = "SHA224"
+    elif (arguments.algorithm.lower() == "all" or arguments.algorithm.lower() == "sha256"):
+        new_hash = hashlib.sha256(testing_string)
+        hash_method = "SHA256"
+    elif (arguments.algorithm.lower() == "all" or arguments.algorithm.lower() == "sha384"):
+        new_hash = hashlib.sha384(testing_string)
+        hash_method = "SHA384"
+    elif (arguments.algorithm.lower() == "all" or arguments.algorithm.lower() == "sha512"):
+        new_hash = hashlib.sha512(testing_string)
+        hash_method = "SHA512"
+
+    # Check to see if the inputted hash matched the new testing hash.
+    # If so set cracked to True, cracked_method to function name and return
+    if (new_hash == arguments.hash):
+        cracked = True
+        cracked_method = "Brute Force"
+        return
 
 # Function to use random strings from the dictionary
 def random_string_attack():
     print ("Random String Attack")
 
 # Function to validate that a integer falls between two values
-def validate_int(min, max):
-    print ("Validate Integer")
+def validate_int(min, max, value):
+    while (value < min or value > max):
+        user_input = input("Invalid entry. Please enter a value between ",min," and ",max,":")
+    return user_input
 
-# Function to validate the wordlist file entered 
-def validate_file_format():
-    print ("Validate File Format")
+# Function to validate if the wordlist file entered, exists and is the right format
+def validate_dictionary(wordlist):
+    # Check if the file enetered exists
+    if (os.path.exists(wordlist) == False):
+        print ("Wordlist file not found. Please enter full path to the chosen wordlist or use the default flag")
+        quit()
+
+    # Seperate file name and extension into array
+    file_attr = os.path.splitext(wordlist)
+    
+    # Extract the file extension
+    file_extension = file_attr[len(file_attr)]
+
+    # Check the file extension matches what is required, in this case a .txt file
+    if (file_extension != ".txt"):
+        print ("Invalid dictionary file extension. Please input a dictionary with a .txt extension")
+        quit()
 
 # Function to display the banner at the top of the cli when first launched
 def display_banner():
@@ -85,7 +150,7 @@ def validate_arguments():
 
     # Check to ensure that the inputted attack method matches one of the supported methods, if not send error message and quit the program
     if (str(arguments.method.lower()) != "dictionary" or str(arguments.method.lower()) != "brute" or str(arguments.method.lower()) != "random" or str(arguments.method.lower()) != str("all")):
-        print ("Invalid method specified, please enter a valid choice from: dictionary, brute, random, all")
+        print ("Invalid method specified, please enter a valid choice from: dictionary, brute, random or all")
         quit()
     # Check to ensure that the inputted number of threads is within the number of threads available for the system running the program, if not send error message and quit the program
     if (arguments.threads.isnumeric() == False or int(arguments.threads) < 1 or int(arguments.threads) > psutil.cpu_count(logical=False)):
@@ -96,23 +161,25 @@ def validate_arguments():
         if (arguments.algorithm.lower() == x):
             hashing_algorithm_found = True
     if (hashing_algorithm_found == False):
-        print ("Invalid hashing method, please enter a valid choice from: MD5, SHA1, SHA224, SHA256, SHA384, SHA512, All")
+        print ("Invalid hashing method, please enter a valid choice from: MD5, SHA1, SHA224, SHA256, SHA384, SHA512 or All")
         quit()
-    # Check to ensure that the inputted dictionary file exists, if not send error message and quit the program
-    if (os.path.exists(arguments.dictionary) == False):
-        print ("Wordlist file not found. Please enter full path to the chosen wordlist or use the default flag")
-        quit()
+    if (arguments.output.lower() != "yes" or arguments.output.lower() != "no"):
+        print ("Invalid output choice specified, default of No has been chosen")
+        arguments.output = "no"
+    # Call the dictionary validation function
+    validate_dictionary(arguments.dictionary)
 
 if __name__ == "__main__":
     # Initialise parser
     parser = argparse.ArgumentParser()
     
-    # Adding optional argument
+    # Adding arguments
     parser.add_argument("-i", "--hash", required=True, help = "Inputted Hash")
-    parser.add_argument("-m", "--method", default = "All", help = "Selected Attack Method")
-    parser.add_argument("-d", "--dictionary", default = "Wordlist.txt", help = "Defined Word Dictionary")
-    parser.add_argument("-t", "--threads", default = "1", help = "Specified Number of Threads (Minimum 1)")
-    parser.add_argument("-a", "--algorithm", default = "All", help = "Selected Hashing Algorithm")
+    parser.add_argument("-m", "--method", default = "All", help = "Selected Attack Method (Default: All)")
+    parser.add_argument("-d", "--dictionary", default = "Wordlist.txt", help = "Defined Word Dictionary (Default: Wordlist.txt)")
+    parser.add_argument("-t", "--threads", default = "1", help = "Specified Number of Threads (Default: 2)")
+    parser.add_argument("-a", "--algorithm", default = "All", help = "Selected Hashing Algorithm (Default: All)")
+    parser.add_argument("-o", "--output", default = "N", help = "Select to display all attempted passwords to cli or not (Default: No)")
     
     # Read arguments from command line
     args = parser.parse_args()
